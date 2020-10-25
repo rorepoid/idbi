946ef575087d2576edf8a2db00fb2271c94e081a
Roberto Orellana <rorepoid@gmail.com>
2020-10-24 23:11:54 -0500

    fix: Auth and header variables (#2)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,14 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
+            @auth
+                @livewire('navigation-dropdown')
+            @endauth
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                    {{ $header ?? config('app.name', 'Laravel') }}
                 </div>
             </header>
 
